862776bf8e649631cccf68c2e6bed91b17c541fd
NitanshSingh <nitanshsinghchauhan@gmail.com>
2026-07-26 22:08:58 +0530

    Delete cpp-practice/PART1 directory

--- cpp-practice/PART1/Practice.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-float a=2/7.0;
-cout<< a;
-}
--- cpp-practice/PART1/Simple_Interest.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    float  principal=540, rate=32, time=3;
-    float si = (principal*rate*time)/100;
-    cout<<"simple interest is : ";
-    cout<< si;
-}
